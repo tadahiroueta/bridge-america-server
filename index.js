395fe8fa1af898a29c7b9d07debc44431fcfda48
tadahiroueta <tadahiroueta@gmail.com>
2023-04-18 00:32:34 -0500

    Added terms

--- index.js
+++ index.js
@@ -4,11 +4,24 @@ const path = require('path');
 const port = 3000;
 const app = require('express')();
 
-app.get('/:filename', (req, res) => {
-  const filePath = path.join(__dirname, 'data', `${req.params.filename}.md`);
+const getTerms = () => {
+    const termExceptions = require('./data/termExceptions.json');
+    return fs.readdirSync(path.join(__dirname, 'data'))
+        .map(file => file.replace('.md', ''))
+        .filter(file => !termExceptions.includes(file));
+}
 
-  if (!fs.existsSync(filePath)) res.status(404).send('File not found');
-  else res.send(fs.readFileSync(filePath, 'utf-8'));
+app.get('/:filename', (req, res) => {
+    const filename = req.params.filename;
+    if (filename === "terms") {
+        res.send(getTerms())
+        return
+    }
+
+    const filePath = path.join(__dirname, 'data', `${req.params.filename}.md`);
+
+    if (!fs.existsSync(filePath)) res.status(404).send('File not found');
+    else res.send(fs.readFileSync(filePath, 'utf-8'));
 });
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
